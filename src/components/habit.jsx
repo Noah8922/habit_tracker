0d4acf0b47744b37a337697187ad53444a958365
Noah8922 <juyeolgo@gmail.com>
2022-05-09 20:10:23 +0900

    make habits component which is th biggest container & give props

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -22,10 +22,11 @@ class Habit extends Component {
     /** 삼항연사자 생각좀 해라!!!! */
   };
   render() {
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
         <button
           className="habit-button habit-increase"
           onClick={this.handleIncreament}
